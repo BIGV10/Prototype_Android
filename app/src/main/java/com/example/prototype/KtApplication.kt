3a46d2cb7ff2dbcfddd28117e2d71c490d9548bc
BIGV10 <BIGV10@mail.ru>
2020-02-07 11:50:48 +0500

    added barcode reader

---
+++ app/src/main/java/com/example/prototype/KtApplication.kt
@@ -0,0 +1,10 @@
+package com.example.prototype
+
+import androidx.multidex.MultiDexApplication
+
+class KtApplication: MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
